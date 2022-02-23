fbc330f34ff23e9fd929acd8f7914c8399c899ae
drayxio <82546547+drayxio@users.noreply.github.com>
2022-02-24 00:39:53 +0100

    Add files via upload

---
+++ gen_invite.py
@@ -0,0 +1,39 @@
+from requests import get, post
+
+account_type = input("account type (bot, user) > ")
+token = input("token > ")
+guild_id = input("guild id > ")
+
+
+print("L'invitations est entrain de se générer...")
+
+if account_type == "bot":
+    auth = {"authorization": "Bot "+token}
+elif account_type == "user":
+    auth = {"authorization": token}
+else:
+    exit()
+
+url = "https://discord.com/api"
+
+me = get(f"{url}/users/@me", headers=auth).json()
+
+try: invite = get(f"{url}/guilds/{guild_id}/invites", headers=auth).json()[0].get("code")
+except:
+
+    for channel in get(f"{url}/guilds/{guild_id}/channels", headers=auth).json():
+        try:
+            invite = post(f"{url}/channels/{channel.get('id')}/invites", headers=auth, json={}).json().get("code")
+            try:
+                " " + invite
+                break
+            except:
+                pass
+        except:
+            invite = None
+
+    if invite:
+
+        input("Invite code generated - " + str(invite))
+    else:
+        input("Can't have an invite code :'(")
