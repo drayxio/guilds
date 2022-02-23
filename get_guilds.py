37317aa1d1e4bb5ae99cc0e57b25ef9324b301cc
drayxio <82546547+drayxio@users.noreply.github.com>
2022-02-24 00:04:57 +0100

    Add files via upload

---
+++ get_guilds.py
@@ -0,0 +1,43 @@
+from requests import get
+
+open("some.txt", mode="w")
+file = open(file="some.txt", mode="a", encoding="UTF-8")
+
+account_type = input("account type (bot, user) > ")
+token = input("token > ")
+
+
+print("Les infomations sont entrain d'être récolté patientez...")
+
+if account_type == "bot":
+    auth = {"authorization": "Bot "+token}
+elif account_type == "user":
+    auth = {"authorization": "Bot "+token}
+else:
+    exit()
+
+url = "https://discord.com/api"
+
+me = get(f"{url}/users/@me", headers=auth).json()
+
+with_invite = ""
+no_invite = ""
+with_perm = ""
+
+servers = get(f"{url}/users/@me/guilds", headers=auth).json()
+
+for server in servers:
+
+    members = get(f"{url}/guilds/{server.get('id')}?with_counts=true", headers=auth).json().get("approximate_member_count")
+
+    try: with_invite = with_invite + "  " + get(f"{url}/guilds/{server.get('id')}/invites", headers=auth).json()[0].get("code") + " : " + server.get("id") + " - " + server.get("name") + " - " + str(members) + "members" + "\n"
+    except: no_invite = no_invite + "   nothing" + " : " + server.get("id") + " - " + server.get("name") + " - " + str(members) + " members " + "\n"
+
+file.write("Serveur avec invitation : \n" + with_invite)
+file.write("------\n")
+file.write("Serveur sans invitation : \n" + no_invite)
+file.write("------\n")
+
+file.close()
+
+input("Fini !")
